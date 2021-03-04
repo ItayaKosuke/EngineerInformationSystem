5c33bdd9c197f102d57ce7ae64a3a2b4bbc35e0b
中川貴輝 <>
2021-03-04 15:26:23 +0900

    ログイン画面のコントローラーとHTMLを追加

---
+++ EIS/src/main/java/com/example/demo/login/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.demo.login;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String getSearch(Model model) {
+		return "view/login/login";
+	}
+	@PostMapping("/login")
+	public String postSearch(Model model) {
+		return "view/login/login";
+	}
+}
