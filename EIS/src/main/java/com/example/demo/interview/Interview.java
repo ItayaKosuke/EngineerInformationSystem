9639fd6a74e0e537a2183c669ed15aca6c1a88fe
板谷誇有介 <>
2021-03-08 15:36:57 +0900

    【詳細画面】画面UIの追加

---
+++ EIS/src/main/java/com/example/demo/interview/Interview.java
@@ -0,0 +1,34 @@
+package com.example.demo.interview;
+
+import lombok.Data;
+
+@Data
+public class Interview {
+
+	private int		interviewNumber;
+	private String	interviewSpeaker;
+	private String	interviewListener;
+	private String	interviewDate;
+	private String	interviewStyle;
+	private String	interviewType;
+	private String	interviewTitle;
+	private String	interviewDetail;
+
+	public Interview(Integer interviewNumber,String interviewSpeaker,String interviewDate,String interviewTitle) {
+		this.interviewNumber 	= interviewNumber;
+		this.interviewSpeaker 	= interviewSpeaker;
+		this.interviewDate 		= interviewDate;
+		this.interviewTitle 	= interviewTitle;
+	}
+	public Interview(Integer interviewNumber,String interviewSpeaker,String	interviewListener,String interviewDate,
+			String interviewStyle,String interviewType,String interviewTitle,String interviewDetail) {
+		this.interviewNumber 	= interviewNumber;
+		this.interviewSpeaker 	= interviewSpeaker;
+		this.interviewListener 	= interviewListener;
+		this.interviewDate 		= interviewDate;
+		this.interviewStyle 	= interviewStyle;
+		this.interviewType		= interviewType;
+		this.interviewTitle 	= interviewTitle;
+		this.interviewDetail	= interviewDetail;
+	}
+}
