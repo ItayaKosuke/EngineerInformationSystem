b709f50b06e72d8519ae1283769a27c5fba08072
板谷誇有介 <>
2021-03-18 09:50:21 +0900

    【情報入力画面】authorをデータベースに登録する機能の追加

--- EIS/src/main/java/com/example/demo/input/InputController.java
+++ EIS/src/main/java/com/example/demo/input/InputController.java
@@ -1,6 +1,8 @@
 package com.example.demo.input;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,7 +34,9 @@ public class InputController {
 							  @RequestParam("title") String title,
 							  @RequestParam("contents") String contents,
 							  Model model) {
-		if(inputRepository.add(speaker, listener, date, method_pulldown, type, title, contents))
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String author = auth.getName();
+		if(inputRepository.add(speaker, listener, date, method_pulldown, type, title, contents, author))
 		{
 			display(model);
 		}
