bc56441a45da4e58f2d92714d5ec217c966369b4
板谷誇有介 <>
2021-03-21 22:12:22 +0900

    【情報入力画面】論理フラグの実装

--- EIS/src/main/java/com/example/demo/input/InputRepository.java
+++ EIS/src/main/java/com/example/demo/input/InputRepository.java
@@ -11,8 +11,8 @@ public class InputRepository {
 
 	public Boolean add(String speaker, String listener, String date, String method_pulldown
 							, String type, String title, String contents, String author) {
-		if(jdbcTemplate.update("INSERT INTO interview_data(INTERVIEW_SPEAKER, INTERVIEW_LISTENER,  INTERVIEW_DATE,INTERVIEW_STYLE, INTERVIEW_TYPE, INTERVIEW_TITLE, INTERVIEW_DETAIL, INTERVIEW_AUTHOR)Values(?,?,?,?,?,?,?,?)",
-														speaker, listener, date, method_pulldown, type, title, contents, author)==1)
+		if(jdbcTemplate.update("INSERT INTO interview_data(INTERVIEW_SPEAKER, INTERVIEW_LISTENER,  INTERVIEW_DATE,INTERVIEW_STYLE, INTERVIEW_TYPE, INTERVIEW_TITLE, INTERVIEW_DETAIL, INTERVIEW_AUTHOR, IS_DELETED)Values(?,?,?,?,?,?,?,?,?)",
+														speaker, listener, date, method_pulldown, type, title, contents, author, false)==1)
 		{
 			return true;
 		}
