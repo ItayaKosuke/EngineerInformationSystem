40fffbbf18a81a36f8640bd27d02baefcc0002c3
板谷誇有介 <>
2021-03-25 13:04:24 +0900

    【編集画面】編集機能の追加

--- EIS/src/main/java/com/example/demo/SecurityConfig.java
+++ EIS/src/main/java/com/example/demo/SecurityConfig.java
@@ -16,17 +16,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
 	@Autowired
 	private DataSource dataSource;
 
-	private static final String USER_SQL =
-			"SELECT"
+	private static final String USER_SQL = "SELECT"
 			+ " LOGIN_ID,"
 			+ " LOGIN_PASS,"
 			+ " true"
@@ -35,8 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			+ " WHERE"
 			+ " LOGIN_ID = ?";
 
-	private static final String ROLE_SQL =
-			"SELECT"
+	private static final String ROLE_SQL = "SELECT"
 			+ " LOGIN_ID,"
 			+ " ROLE"
 			+ " FROM"
@@ -45,57 +44,57 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			+ " LOGIN_ID = ?";
 
 	@Override
-	public void configure(WebSecurity web)throws Exception{
-		web.ignoring().antMatchers("/webjars/**","/css/ **");
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/webjars/**", "/css/ **");
 		// セキュリティ設定無視の設定
-        web.ignoring().antMatchers(
-                "/images/**",
-                "/css/**"  // ←これを追加して解決
-        );
+		web.ignoring().antMatchers(
+				"/images/**",
+				"/css/**" // ←これを追加して解決
+		);
 	}
 
 	@Override
-	protected void configure(HttpSecurity http)throws Exception{
+	protected void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests()
-		.antMatchers("/webjars/ **").permitAll()
-		.antMatchers("/css/ **").permitAll()
-		.antMatchers("/images/ **").permitAll()
-		.antMatchers("/login").permitAll()
-		.antMatchers("/input").permitAll()
-		.antMatchers("/detail").permitAll()
-		.antMatchers("/search").permitAll()
-		.antMatchers("/userMaster").permitAll()
-		.antMatchers("/changePassword").permitAll()
-		.antMatchers("/menu").permitAll()
-		.antMatchers("/temporary").permitAll()
-		.anyRequest().authenticated();
+				.authorizeRequests()
+				.antMatchers("/webjars/ **").permitAll()
+				.antMatchers("/css/ **").permitAll()
+				.antMatchers("/images/ **").permitAll()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/input").permitAll()
+				.antMatchers("/detail").permitAll()
+				.antMatchers("/search").permitAll()
+				.antMatchers("/userMaster").permitAll()
+				.antMatchers("/changePassword").permitAll()
+				.antMatchers("/menu").permitAll()
+				.antMatchers("/temporary").permitAll()
+				.antMatchers("/editSearch").permitAll()
+				.anyRequest().authenticated();
 
 		http
-		.formLogin()
-		.loginProcessingUrl("/login")
-		.loginPage("/login")
-		.failureUrl("/login")
-		.usernameParameter("LOGIN_ID")
-		.passwordParameter("LOGIN_PASS")
-		.defaultSuccessUrl("/menu",true);
+				.formLogin()
+				.loginProcessingUrl("/login")
+				.loginPage("/login")
+				.failureUrl("/login")
+				.usernameParameter("LOGIN_ID")
+				.passwordParameter("LOGIN_PASS")
+				.defaultSuccessUrl("/menu", true);
 
-		http.
-		logout()
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-		.logoutUrl("/logout")
-		.logoutSuccessUrl("/login");
+		http.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login");
 
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
 	}
 
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.usersByUsernameQuery(USER_SQL)
-		.authoritiesByUsernameQuery(ROLE_SQL)
-		.passwordEncoder(passwordEncoder());
+				.dataSource(dataSource)
+				.usersByUsernameQuery(USER_SQL)
+				.authoritiesByUsernameQuery(ROLE_SQL)
+				.passwordEncoder(passwordEncoder());
 	}
 }
